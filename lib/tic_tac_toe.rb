fd97a7b3bea1f571f2a24adea6f32550c6643adb
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-14 18:58:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,7 +97,15 @@ class TicTacToe
  end
   
   def play
-
+  until over?==true
+  turn
+end
+  if won?
+    puts "Congratulations #{winner}!"
+    elsif draw
+    puts "Cat's Game!"
+  end
+end
  
 
 end
